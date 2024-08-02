08fda0880b638a6b6ad53e6d1b95b6da77204628
maryortiz93 <mariela.ortiz@espoch.edu.ec>
2024-08-01 20:57:53 -0500

    metodos actualizados

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -12,24 +12,45 @@ export class OrdersService {
   ) {}
 
   async processOrder(processOrderDto: ProcessOrderDto) {
-    /// creo la orden
-    const order = this.orderRepository.create({
+    let delivery = 0;
+
+    if (processOrderDto.distance < 1) {
+      delivery = 1;
+    } else if (processOrderDto.distance < 5) {
+      delivery = 3;
+    } else if (processOrderDto.distance < 10) {
+      delivery = 8;
+    } else if (processOrderDto.distance < 15) {
+      delivery = 15;
+    } else if (processOrderDto.distance < 20) {
+      delivery = 25;
+    } else {
+      throw new Error('Unprocessable entry');
+    }
+
+    // registrar
+
+    const orderDataForInsert = {
       id: processOrderDto.orderId,
       userId: processOrderDto.userId,
       value: processOrderDto.value,
       distance: processOrderDto.distance,
-    });
-
-    order.calculateDelivery();
-
-    await this.orderRepository.insert(order);
-
-    return {
-      id: processOrderDto.orderId,
-      userId: processOrderDto.userId,
-      value: processOrderDto.value,
-      distance: processOrderDto.distance,
-      delivery: order.delivery,
+      delivery: delivery,
     };
+
+    let order = this.orderRepository.create(orderDataForInsert);
+    await this.orderRepository.save(order);
+
+    if (!order) {
+      throw new Error('Unprocessable entry');
+    }
+
+    console.log('Order create with id: ' + order.id);
+
+    return order;
+  }
+
+  getOrderById(orderId: string) {
+    return this.orderRepository.findOneBy({ id: orderId });
   }
 }
